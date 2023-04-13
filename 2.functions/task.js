6647d703d70ef18b10cac89189fad4908a4f54cf
Danvintius <danila.94@mail.ru>
2023-04-13 18:11:30 +0300

    Первое задание готово

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,14 +1,55 @@
 function getArrayParams(...arr) {
-
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    sum += arr[i];
+  }
+  let avg2 = (sum / arr.length).toFixed(2);
+  let avg = Number(avg2);
+  let result = {
+    min:  min,
+    max: max,
+    avg: avg,
+  }
+console.log(result)
   return { min: min, max: max, avg: avg };
 }
 
 function summElementsWorker(...arr) {
-
+  let sum = 0;
+  for (i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  console.log(sum);
+  if (arr = null) {
+    console.log(0);
+  }
 }
 
 function differenceMaxMinWorker(...arr) {
-
+  let min = Infinity;
+  let max = -Infinity;
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+}
+  let difference = max - min;
+  if (arr = null) {
+    console.log(0);
+  } else {
+    console.log(difference);
+  }
 }
 
 function differenceEvenOddWorker(...arr) {
