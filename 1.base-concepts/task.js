187b296f1d3419384cc35f2caae53844c88fba55
Danvintius <danila.94@mail.ru>
2023-04-13 12:02:48 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,9 +1,23 @@
-"use strict"
-function solveEquation(a, b, c) {
+"use sctrict";
+
+function solveEquation(a,b,c) {
   let arr = [];
-  
-  return arr;
+  let d = Math.pow(b,2) -4 * a * c;
+  if (d < 0) {
+    arr = [];
+    console.log(arr);
+  }
+  if (d == 0) {
+    arr[0] = -b/(2*a);
+    console.log(arr);
+  }
+  if (d > 0) {
+    arr[0] = (-b + Math.sqrt(d,2))/(2*a);
+    arr[1] = (-b - Math.sqrt(d,2))/(2*a);
+    console.log(arr);
+  }
 }
+
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
