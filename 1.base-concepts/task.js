6dcafb74364ef696319e50e6b7bb8370ddc56789
Danvintius <danila.94@mail.ru>
2023-04-15 15:37:01 +0300

    Внесены небольшие исправления

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -20,10 +20,10 @@ function solveEquation(a,b,c) {
 
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  if (percent > 0 && percent <= 100) {
+  if (typeof percent === "number") {
     let P = percent / 100 / 12;
     let S = amount - contribution;
-    let monthPay= S * (P + (P / ((Math.pow(1 + P, countMonth)) - 1)));
+    let monthPay= S * (P + (P / ((Math.pow(1 + P, countMonths)) - 1)));
     let result = Number((monthPay * countMonths).toFixed(2));
     return result;
   } else {
