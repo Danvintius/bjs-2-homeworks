2b2a350f58d963217fb128f9e343ced84bccd3d7
Danvintius <danila.94@mail.ru>
2023-04-15 15:11:37 +0300

    Внесены исправления в первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -5,16 +5,16 @@ function solveEquation(a,b,c) {
   let d = Math.pow(b,2) -4 * a * c;
   if (d < 0) {
     arr = [];
-    console.log(arr);
+    return arr;
   }
   if (d == 0) {
     arr[0] = -b/(2*a);
-    console.log(arr);
+    return arr;
   }
   if (d > 0) {
     arr[0] = (-b + Math.sqrt(d,2))/(2*a);
     arr[1] = (-b - Math.sqrt(d,2))/(2*a);
-    console.log(arr);
+    return arr;
   }
 }
 
@@ -24,8 +24,8 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
     let P = percent / 100 / 12;
     let S = amount - contribution;
     let monthPay= S * (P + (P / ((Math.pow(1 + P, countMonth)) - 1)));
-    let result = (monthPay * countMonth).toFixed(2);
-    console.log(result);
+    let result = Number((monthPay * countMonths).toFixed(2));
+    return result;
   } else {
     return false;
   }
