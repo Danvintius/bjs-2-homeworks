1f432cdba09ad7340e4a9058cc22fb8d1334098d
Danvintius <danila.94@mail.ru>
2023-04-22 01:45:30 +0300

    Пятое задание полностью готово

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -74,12 +74,12 @@ class Library {
 
   giveBookByName(bookName) {
     for (let i = 0; i < this.books.length; i++) {
-      if (this.books[i].name == bookName) {
-        this.books.delete(this.books[i]);
-        return this.books[i].name;
-      } else {
-        return null;
-      }
+      if (this.books[i].name === bookName) {
+        return this.books.splice([i], 1)[0];
+      } 
     }
+      return null;
   }
 }
+
+
